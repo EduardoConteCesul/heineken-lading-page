447e0a8baef9714dae856a62e0242003a2536c2b
Eduardo Conte <eduardo.conte.dev@gmail.com>
2026-03-26 20:30:13 -0300

    feat: criado container com imagem e botão: Confira Nossas Vagas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,90 @@
-import { Image as ChakraImage, HStack, VStack } from "@chakra-ui/react";
+import { Image as ChakraImage, HStack, Text, VStack } from "@chakra-ui/react";
 import NextImage from "next/image";
 
 import bannerFooter from "../../public/assets/bannerFooter.jpeg";
 
 export default function Home() {
-
-
   return (
     <VStack as="main" gap={0} >
-      {/* <HStack as="section">
-        <ChakraImage asChild>
-          <NextImage
-            src={bannerFooter}
-            alt="Amigos abraçados segurando Heineken"
-            fill
-            style={{
-              objectFit: "cover",
-            }} />
-        </ChakraImage>
-      </HStack> */}
+      <VStack as="section" w="100%">
+
+        {/* Penultimo Banner */}
+        <HStack position="relative" w="100%" h="350px">
+
+          <ChakraImage w="100%" maxH="350px" asChild>
+            <NextImage
+              src={bannerFooter}
+              alt="..."
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </ChakraImage>
+
+          <VStack
+            position="absolute"
+            inset="0"
+            justify="center"
+            align="center"
+            zIndex="1"
+          >
+            <VStack
+              maxW="720px"
+              w="100%"
+              h="128px"
+              fontFamily="'HEINEKEN Core', sans-serif"
+              fontSize="16px"
+              lineHeight="24px"
+              zIndex="2"
+              gap="0"
+            >
+              <Text
+                as="h2"
+                color="white"
+                fontSize="56px"
+                fontWeight="700"
+                lineHeight="56px"
+                textAlign="center"
+                w="720px"
+                m="8px 0"
+              >
+                Faça parte do nosso time de estrelas
+              </Text>
+
+              <VStack
+                mt="16px"
+                maxW="480px"
+                w="100%"
+                h="48px"
+                align="center"
+                justify="center"
+              >
+                <HStack
+                  as="a"
+                  bg="rgb(32, 85, 39)"
+                  color="white"
+                  fontFamily="'HEINEKEN Core', sans-serif"
+                  fontSize="16px"
+                  fontWeight="bold"
+                  h="48px"
+                  px="16px"
+                  w="220px"
+                  justify="center"
+                  align="center"
+                  borderRadius="0"
+                  textDecoration="none"
+                  transition="all 0.15s ease-in-out"
+                  _hover={{
+                    bg: "rgb(28, 75, 34)",
+                    textDecoration: "none"
+                  }}
+                >
+                  Confira Nossas Vagas
+                </HStack>
+              </VStack>
+            </VStack>
+          </VStack>
+        </HStack>
+      </VStack>
     </VStack>
   )
 }
